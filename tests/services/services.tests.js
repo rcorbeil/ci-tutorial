1d09480ed3cf1dc70a8637c6521166c0be252d20
rcorbeil <rcorbeil@softstackfactory.com>
2016-02-29 03:11:08 +0000

    add answers test

--- tests/services/services.tests.js
+++ tests/services/services.tests.js
@@ -1,4 +1,6 @@
 describe('Services', function(){
+  
+    //Test the sort routine
     var sortSvcObj;
     
     // load the service's module 
@@ -7,11 +9,33 @@ describe('Services', function(){
       sortSvcObj=sortSvc;
     }));
 
-    
     // tests start here
-    it('sorts in ascending order by default', function() {
+    it('sorts in ascending order', function() {
     var users = ['robin', 'ryn', 'shawn', 'piper', 'lark', 'per', 'teal'];
     var sorted = sortSvcObj.bubbleSort(users);
     expect(sorted).toEqual(['lark', 'per', 'piper', 'robin', 'ryn', 'shawn', 'teal']);
-  });
+    });
+  
+    //Test the TKanswers routine
+    var answersSvcObj;
+    beforeEach(module('TKServicesModule'));
+    beforeEach(inject(function( TKAnswersService ){
+      answersSvcObj=TKAnswersService;
+    }));
+    
+    it('sets and gets the answer categories', function() {
+        var answerCategories = {
+            "competing": 6,
+            "collaborating": 5,
+            "compromising": 4,
+            "avoiding": 10,
+            "accommodating": 12
+        };
+        
+        answersSvcObj.setAnswers(answerCategories);
+        var gottenAnswers = {};//answersSvcObj.getAnswers();
+        
+        expect(gottenAnswers).toEqual(answerCategories);
+    });
+      
 });
