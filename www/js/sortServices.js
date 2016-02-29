db30e8cd318e2c7f5cf25b7c9908ba0171c2c63c
rcorbeil <rcorbeil@softstackfactory.com>
2016-02-29 02:00:46 +0000

    add sort logic

--- www/js/sortServices.js
+++ www/js/sortServices.js
@@ -1,7 +1,19 @@
 angular.module('sortServices', [])
   .service('sortSvc', [function(){
-    this.bubbleSort = function(unSorted){
-        var sorted = [];
+    this.bubbleSort = function(unsortedArray){
+        var sortedArray = unsortedArray;
+        var swapped = false;
+        do {
+          for( var i=0; i<sortedArray.length-2; i++){
+            if( sortedArray[i].localeCompare(sortedArray[i+1]) > 0) {
+              var temp = sortedArray[i];
+              sortedArray[i] = sortedArray[i+1];
+              sortedArray[i+1] = temp;
+              
+              swapped = true;
+            }
+          }
+        }while(swapped);
         
         return sorted;
     };
